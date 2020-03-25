8f17a16cbd459a5320f2586aff438fbf05fe92a2
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-03-25 13:04:09 +0530

    Update hello_javascript.js

--- Programming in Javascript/hello_javascript.js
+++ Programming in Javascript/hello_javascript.js
@@ -5,6 +5,16 @@ let b= [1,2,3,4,5,6];
 console.log(b);
 console.log("hello World");
 
+/* OUTPUT:
+13
+[ 1, 2, 3, 4, 5, 6 ]
+hello World
+
+
+Download node and run on cmd by
+node filename 
+
+*/
 
 
 
