9115c4cf038dbfb1ebc367cf595647a9cca3f912
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-03-26 11:55:29 +0530

    added files

---
+++ Programming in Javascript/hello_javascript2.js
@@ -0,0 +1,49 @@
+let a=13;
+console.log(a);
+
+let b= [1,2,3,4,5,6];
+console.log(b);
+console.log("hello World");
+
+c = 20; //global variable
+var d = 30; //function Scope
+let e = 50; //block Scope 
+
+//let has a block scope and var has a function scope 
+
+function fun(){
+    let a=5;
+    if(a===5){
+     let b =10;  //block Scope
+        console.log("inside the function",b);
+
+    }
+    console.log("outside ",b);
+}
+function fun1(){
+    let a=5; 
+    if(a===5){
+     let f =10;  //block Scope
+        console.log("inside the function",f);
+
+    }
+    console.log("outside ",f);
+}
+function fun2(){
+    let a=5;
+    if(a===5){
+     var b =10;//function Scope
+        console.log("inside the function",b);
+
+    }
+    console.log("outside ",b);
+}
+function fun3(){
+    let a=5;
+    if(a===5){
+     f =10;//Global variable
+        console.log("inside the function",f);
+
+    }
+    console.log("outside ",f);
+}
