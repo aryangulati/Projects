408ba38381505b13440303f86fe82505c5a565ff
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-04-08 23:27:06 +0530

    added

---
+++ Mini_Games/snake.js
@@ -0,0 +1,24 @@
+function init(){
+    var canvas = document.getElementById("mycanvas");
+    W=H=canvas.width = canvas.height = 1000;
+
+}
+
+
+function(){
+//console.log("In Draw");
+
+}
+
+function update(){
+    //console.log("In Update");
+}
+
+function gameloop(){
+
+}
+
+init();
+
+
+var f= setInterval(gameloop,100);
