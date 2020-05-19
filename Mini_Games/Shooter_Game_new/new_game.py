9ba8bc53a73c176e4f02e7942dd2165f2f2f3295
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-05-19 23:34:09 +0530

    added comment

--- Mini_Games/Shooter_Game_new/new_game.py
+++ Mini_Games/Shooter_Game_new/new_game.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 # create a window  what we draw on
 #here u have to give the size of window 
-win=pygame.display.set_mode((500,500))
+win=pygame.display.set_mode((1000,600))
 # to give caption on the window screen 
 pygame.display.set_caption("New Game")
 #few attributes of our chachters
@@ -14,14 +14,24 @@ width = 40
 height = 60
 vel= 5
 
+#start by making run variable
 run= True
+
 while run:
+    #using delay so making clk in pygame so things don't happen quick
     pygame.time.delay(100)
+    #anything (events )that happens in pygame
+    #moving keys and  mouse positions and we can loop through all the events
     for event in pygame.event.get() :
+        #we gonna check if we have clicked exit button on window
+        #if we click exit button on window
         if event.type==pygame.QUIT:
+            #break the loop
             run = False
 
-pygame.quit()            
+#ends the program and closes window for us 
+pygame.quit()
+
 
 
 
