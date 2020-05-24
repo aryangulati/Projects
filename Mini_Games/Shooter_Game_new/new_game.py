80a21c7963319dccac3f428be04674286cc2d759
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-05-24 21:53:52 +0530

    .

--- Mini_Games/Shooter_Game_new/new_game.py
+++ Mini_Games/Shooter_Game_new/new_game.py
@@ -14,6 +14,7 @@ pygame.display.set_caption("New Game")
 #if u have put the images in some folder then u need to use pygame.image.load(pygame.path.join("your path",'R1.png'))
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+
 # background image
 bg = pygame.image.load('bg.jpg')
 #standing image of our char in the game 
