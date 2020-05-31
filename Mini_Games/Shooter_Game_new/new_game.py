e907ab7fd6a59d360598c719f5b56e6fe308edff
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-05-31 22:49:42 +0530

    .

--- Mini_Games/Shooter_Game_new/new_game.py
+++ Mini_Games/Shooter_Game_new/new_game.py
@@ -15,8 +15,6 @@ pygame.display.set_caption("New Game")
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 
-
-
 # background image
 bg = pygame.image.load('bg.jpg')
 
