86d6fdf1ec09030def897e01bcf2accaf94dfaed
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-08-13 16:09:58 +0530

    .

---
+++ PYTHON/angle_Detector/angle.py
@@ -0,0 +1,20 @@
+import cv2 
+import math 
+
+
+path="acute-angles.png"
+img=cv2.imread(path)
+pointlist=[]
+
+
+def getangle(pointlist):
+    p1,p2,p3=pointlist[-3:]
+    m1=
+
+
+
+while True:
+    if len(pointlist)%3==0 and len(pointslist)!=0:
+        getangle(pointlist)
+
+    cv2.imshow("Image",img)    
