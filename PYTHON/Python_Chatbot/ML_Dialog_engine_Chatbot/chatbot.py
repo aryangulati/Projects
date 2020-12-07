6ed8d88ddab4f6117b89ae37c2399683769a32fb
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-12-08 00:23:22 +0530

    Update chatbot.py

--- PYTHON/Python_Chatbot/ML_Dialog_engine_Chatbot/chatbot.py
+++ PYTHON/Python_Chatbot/ML_Dialog_engine_Chatbot/chatbot.py
@@ -1,2 +1,17 @@
-from chatterbot import chatterbot
-from chatterbot.trainers import ChatterBotCorpus
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+bot=ChatBot("Dell bot") #Dell bot name object of bot
+
+trainer= ChatterBotCorpusTrainer(bot)
+
+trainer.train("chatterbot.corpus.english") #it train from english language
+
+while True:
+    query = str(input(">> "))
+    print(bot.get_response(query))
+
+    if "exit" in query:
+        break
+
+
