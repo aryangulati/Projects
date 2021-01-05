590976d53d6f2e26c56297b66adc584408c0072d
aryangulati <42711978+aryangulati@users.noreply.github.com>
2021-01-06 00:12:57 +0530

    Update security_camera.py

--- PYTHON/Python_Body_Movements/security_camera.py
+++ PYTHON/Python_Body_Movements/security_camera.py
@@ -12,7 +12,11 @@ while True:
         diff =cv2.absdiff(frame,frame1)
         gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
         #their are diff type of blurr but here we are using gaussian Blur
-        blur= cv2.GaussianBlur(gray,(5,5),0)
+        blur= cv2.Gauss ianBlur(gray,(5,5),0)
+        _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+        dilated = cv2.dilate(thresh, None, iterations=3)
+        contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
 
         cv2.imshow("Absolute diffrence b/w both the frames", diff)  
 
