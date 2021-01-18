90f6aac8439df4e6a208d77e81ead4a45b304059
aryangulati <42711978+aryangulati@users.noreply.github.com>
2021-01-19 00:14:42 +0530

    Update pdf_reader.py

--- PYTHON/Pdf_Reader/pdf_reader.py
+++ PYTHON/Pdf_Reader/pdf_reader.py
@@ -3,10 +3,10 @@ import PyPDF2
 
 pdf= open("Andriy_Burkov.pdf",'rb')
 
-pdfreader = PyPDF2.PdfFileReader(pdf)
+pdfreader = PyPDF2.PdfFileReader(pdf,strict=False)
 
 pages=pdfreader.numPages
-print(pages)
+# print(pages) you can used this to print number of pages 
 
 
 dell= pyttsx3.init()
