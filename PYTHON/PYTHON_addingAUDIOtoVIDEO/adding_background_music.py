409ca1b1fde712bd192703003c8c253c3384a6a4
aryangulati <42711978+aryangulati@users.noreply.github.com>
2021-02-14 22:44:17 +0530

    added new project

--- PYTHON/PYTHON_addingAUDIOtoVIDEO/adding_background_music.py
+++
@@ -1,6 +0,0 @@
-import moviepy.editor as mpe
-clip = mpe.VideoFileClip("input/Episode 5.mp4")
-audio_bg = mpe.AudioFileClip("input/shakiraHips.mp3")
-final_audio = mpe.CompositeAudioClip([audio_bg, clip.audio])
-final_clip = clip.set_audio(final_audio)
-final_clip.write_videofile("out/output.mp4")
--- PYTHON/PYTHON_addingAUDIOtoVIDEO/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-import moviepy.editor as mpe
-clip = mpe.VideoFileClip("Input/capture.webm")
-audio_bg = mpe.AudioFileClip("Input/aryan3.mp3")
-final_audio = mpe.CompositeAudioClip([audio_bg, clip.audio])
-final_clip = clip.set_audio(final_audio)
-final_clip.write_videofile("out/output.mp4")
